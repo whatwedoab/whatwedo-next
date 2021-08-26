ade5288ec2294d3061900e0488b07c7ffe091e0c
Mikael Edgren <mikael.edgren@whatwedo.se>
2021-08-26 14:50:12 +0200

    Culinary Canvas page re-styling

--- Portfolio/CulinaryCanvas/CulinaryCanvas.tsx
+++ Portfolio/CulinaryCanvas/CulinaryCanvas.tsx
@@ -1,9 +1,7 @@
 import React, { useCallback, useRef, useState } from 'react'
 import s from './CulinaryCanvas.module.scss'
 import { useWindowSize } from '../../services/useWindowSize'
-import { ImageMosaic } from '../../components/ImageMosaic/ImageMosaic'
 import { Tags } from '../../components/Tags/Tags'
-import { ParallaxImages } from '../../components/ParallaxImages/ParallaxImages'
 import { motion, Variants } from 'framer-motion'
 import { Image } from '../../components/Image/Image'
 import { useInView } from 'react-intersection-observer'
@@ -13,6 +11,7 @@ import { useActiveNav } from '../../services/useActiveNav'
 import { COLOR } from '../../styles/COLOR'
 import { PAGE_IN_VIEW_OPTIONS } from '../../services/App.context'
 import { useOffsets } from '../../services/useOffsets'
+import Link from 'next/link'
 
 const parallaxImages = [
   {
@@ -53,7 +52,7 @@ export default function CulinaryCanvas() {
   }
 
   return (
-    <motion.article className={s.article} ref={setRefs}>
+    <article className={s.article} ref={setRefs}>
       <motion.div
         layoutId="triangle"
         className={s.triangle}
@@ -61,40 +60,128 @@ export default function CulinaryCanvas() {
         animate={{ y: 0 }}
         transition={{ type: 'tween', ease: 'linear' }}
       />
-      <motion.section
-        className={s.logoContainer}
-        initial="hidden"
-        animate={logoLoaded ? 'visible' : 'hidden'}
-        transition={{ type: 'tween', duration: 7, ease: 'anticipate' }}
-        variants={logoVariants}
-      >
-        <a
-          href="https://culinary-canvas.com"
-          title="Culinary Canvas"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Image
-            alt="Culinary Canvas logo"
-            objectFit="contain"
-            layout="fill"
-            src="/assets/culinary-canvas-logo.svg"
-            priority
-            onLoadingComplete={() => setLogoLoaded(true)}
-          />
-        </a>
-      </motion.section>
-
-      <Tags
-        delay={1.5}
-        tags={['logo', 'branding', 'web design', 'web development', 'CMS']}
-        containerClassName={s.tagsContainer}
-      />
-
-      <section className={s.parallaxContainer}>
-        {!!height && <ParallaxImages images={parallaxImages} />}
+      <section className={s.nameContainer}>
+        <Image
+          alt="Culinary Canvas logo"
+          objectFit="contain"
+          layout="fill"
+          src="/assets/culinary-canvas/culinary-canvas-logo.svg"
+          priority
+          containerClassName={s.logoContainer}
+          onLoadingComplete={() => setLogoLoaded(true)}
+        />
+        <Tags
+          delay={1.5}
+          tags={['logo', 'branding', 'web design', 'web development', 'CMS']}
+          containerClassName={s.tagsContainer}
+        />
       </section>
 
+      <section className={s.textContainer}>
+        <p>
+          Culinary Canvas celebrates the creativity of culinary and beverage
+          professionals around the world. High-end editorial content with
+          vibrant and eye-catching photography.
+        </p>
+      </section>
+
+      <section className={s.mockup1Container}>
+        <Image
+          src={'/assets/culinary-canvas/culinary-canvas-spreads-1.png'}
+          alt="Culinary Canvas visual identity"
+          layout="fill"
+          objectFit={'contain'}
+        />
+      </section>
+
+      <section className={s.textContainer}>
+        <p>
+          A modern brand, with lots of space for creativity. The lower stripe in
+          the mark represents the shelf of an easel; the space between the shelf
+          and the C symbolises the blank canvas. The logotype is made with the
+          Sofia Pro type and sports all capital letters with a wide
+          letter-spacing.
+        </p>
+      </section>
+
+      <section className={s.mockup1Container}>
+        <Image
+          src={'/assets/culinary-canvas/culinary-canvas-debossed.jpg'}
+          alt="Culinary Canvas"
+          layout="fill"
+          objectFit="cover"
+        />
+      </section>
+
+      <section className={s.textContainer}>
+        <p>
+          The imagery is the star content of Culinary Canvas. Emphasis on the
+          layout is therefore to give the photos room to shine. This is coupled
+          with type faces that are picked to give a modern feel, while also
+          offering diversity enough to create expressive article layouts.
+        </p>
+      </section>
+
+      <section className={s.mockup1Container}>
+        <Image
+          src={'/assets/culinary-canvas/culinary-canvas-web-1.png'}
+          alt="Culinary Canvas web site"
+          layout="fill"
+          objectFit="cover"
+        />
+      </section>
+
+      <section className={s.textContainer}>
+        <p>
+          Culinary Canvas runs in the cloud. This allows the site to scale
+          without worrying about hardware capacity and limitations, but instead
+          leverage the awesome computing power offered by the dragons of the
+          tech industry.
+        </p>
+        <p>
+          The content, the articles, are created with our own custom made CMS.
+          It&apos;s tailor made to fit the current needs while giving us the
+          full control to adjust and tinker it to future needs.
+        </p>
+      </section>
+
+      <section className={s.mockup2Container}>
+        <Image
+          src={'/assets/culinary-canvas/culinary-canvas-web-mobile-1.png'}
+          alt="Culinary Canvas web site"
+          layout="fill"
+          objectFit="cover"
+        />
+        <Image
+          src={'/assets/culinary-canvas/culinary-canvas-magazine-1.png'}
+          alt="Culinary Canvas"
+          layout="fill"
+          objectFit="cover"
+        />
+      </section>
+
+      <section className={s.textContainer}>
+        <p>
+          Visit the Culinary Canvas website at{' '}
+          <a
+            href="https://www.culinary-canvas.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            www.culinary-canvas.com
+          </a>{' '}
+          to see it in action.
+        </p>
+        <p>
+          <i>
+            <Link href="/contact">Contact me</Link> if you&apos;re interested in
+            learning more about the stack and the technical solution.
+          </i>
+        </p>
+      </section>
+    </article>
+
+    /*
       <h3>The brief</h3>
       <section className={s.contentContainer}>
         <p>
@@ -154,6 +241,7 @@ export default function CulinaryCanvas() {
       <section className={s.contentContainer}>
         <ImageMosaic hrefs={mosaicHrefs} />
       </section>
-    </motion.article>
+    </article>
+*/
   )
 }
